762a090f32b42fca833440f11fa7ad8d55406288
codyfet <codyfet@mail.ru>
2020-06-13 01:52:19 +0300

    Добавил hero изображение

--- client/src/Pages/Main.js
+++ client/src/Pages/Main.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {Image} from "semantic-ui-react";
+import {getRandomInt} from "../Utils/Utils";
 
 /**
  * Главная страница приложения.
  */
 export const Main = () => {
-    return <div>Здесь будет главная страница</div>;
+    return (
+        <div className="hero">
+            <Image src={`../src/Assets/films/${getRandomInt(1, 3)}.jpg`} fluid />
+        </div>
+    );
 };
