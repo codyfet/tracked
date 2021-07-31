d5d01923c1dc216bfb5f86f34118e99e3af12385
codyfet <codyfet@mail.ru>
2021-07-31 21:12:43 +0300

    Поднял версии пакетов (вебпак и прочие)

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssestsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
@@ -12,35 +12,37 @@ const isProd = !isDev;
  * Возвращает optimization конфиг в зависимости от режима сборки.
  */
 const getOptimization = () => {
-    const config = {
-        splitChunks: {
-            chunks: "all"
-        }
-    };
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
 
-    if (isProd) {
-        config.minimizer = [
-            new OptimizeCssAssestsWebpackPlugin(),
-            new TerserWebpackPlugin(),
-        ];
-    }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssestsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
 
-    return config;
+  return config;
 };
 
 /**
  * Возвращает конфиг для обработки js|jsx файлов в зависимости от режима сборки.
  */
 const getJsLoaders = () => {
-    const loaders = [{
-        loader: "babel-loader"
-    }];
+  const loaders = [
+    {
+      loader: "babel-loader",
+    },
+  ];
 
-    if (isDev) {
-        loaders.push("eslint-loader");
-    }
+  if (isDev) {
+    loaders.push("eslint-loader");
+  }
 
-    return loaders;
+  return loaders;
 };
 
 /**
@@ -50,29 +52,31 @@ const getJsLoaders = () => {
  * @param {string[]} extraLoaders Массив дополнительных лоадеров (например для обработки less).
  */
 const getCssLoaders = (extraLoaders) => {
-    const loaders = [
-      {
-        loader: MiniCssExtractPlugin.loader,
-        options: {
-          hmr: isDev,
-          reloadAll: true
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true,
+      },
+    },
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: () => [require("autoprefixer")],
         },
       },
-      "css-loader",
-      {
-        loader: "postcss-loader",
-        options: {
-            plugins: () => [require("autoprefixer")],
-        }
-    }
-    ];
+    },
+  ];
 
-    if (extraLoaders) {
-      loaders.push(extraLoaders);
-    }
+  if (extraLoaders) {
+    loaders.push(extraLoaders);
+  }
 
-    return loaders;
-  };
+  return loaders;
+};
 
 /**
  * Возвращает имя файла с хэшем, либо без - в зависимости от режима сборки.
@@ -80,68 +84,68 @@ const getCssLoaders = (extraLoaders) => {
  * @param {string} extension Расщирение.
  */
 const getDistFilename = (extension) => {
-    return isDev ? `[name].${extension}` : `[name].[hash].${extension}`;
+  return isDev ? `[name].${extension}` : `[name].[hash].${extension}`;
 };
 
 module.exports = {
-    context: path.resolve(__dirname, "src"),
-    entry: ["@babel/polyfill", "./index.js"],
-    output: {
-        filename: getDistFilename("js"),
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "/",
+  context: path.resolve(__dirname, "src"),
+  entry: ["@babel/polyfill", "./index.js"],
+  output: {
+    filename: getDistFilename("js"),
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    port: 4210,
+    hot: isDev,
+    historyApiFallback: true,
+    proxy: {
+      "/api/**": {
+        target: "http://localhost:5000",
+        secure: false,
+      },
     },
-    devServer: {
-        port: 4210,
-        hot: isDev,
-        historyApiFallback: true,
-        proxy: {
-            "/api/**": {
-                target: "http://localhost:5000",
-                secure: false
-            }
-        }
-    },
-    devtool: isDev ? "source-map" : "",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: getJsLoaders()
-            },
-            {
-                test: /\.css$/,
-                use: getCssLoaders()
-            },
-            {
-                test: /\.less$/,
-                use: getCssLoaders("less-loader")
-            },
-            {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ["file-loader"]
-            },
-            {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                use: ["file-loader"]
-            },
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".css", ".less", ".png"]
-    },
-    optimization: getOptimization(),
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-            minify: {
-                collapseWhitespace: isProd
-            }
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: getDistFilename("css"),
-        })
-    ]
+  },
+  devtool: isDev ? "source-map" : "",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: getJsLoaders(),
+      },
+      {
+        test: /\.css$/,
+        use: getCssLoaders(),
+      },
+      {
+        test: /\.less$/,
+        use: getCssLoaders("less-loader"),
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".css", ".less", ".png"],
+  },
+  optimization: getOptimization(),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: getDistFilename("css"),
+    }),
+  ],
 };
