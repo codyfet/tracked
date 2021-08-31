9cfef587fa5478d83b591e0cf5de39869c17c9d2
codyfet <codyfet@mail.ru>
2021-08-31 21:19:52 +0300

    Вынес record в отдельный файл-контроллер

---
+++ server/src/controllers/record.controller.ts
@@ -0,0 +1,42 @@
+import {RecordModel} from "../models/Record";
+import {Request, Response} from "express";
+import {FilterQuery} from "mongoose";
+import {IRecordModel} from "../interfaces/Record";
+import asyncHandler from "express-async-handler";
+
+/**
+ * @desc    Возвращает список записей.
+ * @route   GET /api/record.
+ * @access  Public
+ */
+const getRecords = asyncHandler(async (req: Request, res: Response) => {
+    const filter: FilterQuery<IRecordModel> = {
+        userId: req.query.userId as string,
+        viewdate: {
+            $gte: new Date(req.query.year ? +req.query.year : 0, 0, 1),
+            $lt: new Date(req.query.year ? +req.query.year : 0, 11, 31),
+        },
+    };
+
+    if (req.query.types) {
+        filter.type = {$in: req.query.types as string[]};
+    }
+
+    const records = await RecordModel.find(filter).sort(req.query.sortBy).exec();
+
+    if (records) {
+        // res.status(401);
+        // throw new Error("Not Authorized");
+        res.status(201).json(records);
+    } else {
+        /**
+         * Достаточно просто выкинуть ошибку и asyncHandler прокинет ее в кастомную middleware (см. errorMiddleware),
+         * которая отправит ошибку на ui в формате json. По дефолту отправится 500 ошибка, если нужно переопределить номер,
+         * то достаточно добавить перед выкидыванием ошибки
+         * res.status(404).
+         */
+        throw new Error("Что-то пошло не так...");
+    }
+});
+
+export {getRecords};
--- server/src/routes/record.routes.ts
+++ server/src/routes/record.routes.ts
@@ -7,7 +7,7 @@ import async from "async";
 import express, {Request, Response, Router} from "express";
 import {FilterQuery} from "mongoose";
 import {IRecordModel} from "../interfaces/Record";
-import asyncHandler from "express-async-handler";
+import {getRecords} from "../controllers/record.controller";
 
 const router: Router = express.Router();
 
@@ -154,42 +154,7 @@ router.delete("/:id/delete", verifyToken, async (req: Request, res: Response) =>
     }
 });
 
-/**
- * @desc    Возвращает список записей.
- * @route   GET /api/record.
- * @access  Public
- */
-router.get(
-    "/",
-    asyncHandler(async (req: Request, res: Response) => {
-        const filter: FilterQuery<IRecordModel> = {
-            userId: req.query.userId as string,
-            viewdate: {
-                $gte: new Date(req.query.year ? +req.query.year : 0, 0, 1),
-                $lt: new Date(req.query.year ? +req.query.year : 0, 11, 31),
-            },
-        };
-
-        if (req.query.types) {
-            filter.type = {$in: req.query.types as string[]};
-        }
-
-        const records = await RecordModel.find(filter).sort(req.query.sortBy).exec();
-
-        if (records) {
-            // res.status(401);
-            // throw new Error("Not Authorized");
-            res.status(201).json(records);
-        } else {
-            /**
-             * Достаточно просто выкинуть ошибку и asyncHandler прокинет ее в кастомную middleware (см. errorMiddleware),
-             * которая отправит ошибку на ui в формате json. По дефолту отправится 500 ошибка, если нужно переопределить номер,
-             * то достаточно добавить перед выкидыванием ошибки
-             * res.status(404).
-             */
-            throw new Error("Что-то пошло не так...");
-        }
-    })
-);
+// TODO: Сделать остальные методы через контроллер аналогично этому.
+router.route("/").get(getRecords);
 
 module.exports = router;
