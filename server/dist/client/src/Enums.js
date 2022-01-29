a54efbb93f62df604a5c1e50b0d32cd25ab508e9
codyfet <codyfet@mail.ru>
2022-01-29 21:39:35 +0300

    Удалил содержимое server/dist

--- server/dist/client/src/Enums.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ERecordType = void 0;
-var ERecordType;
-(function (ERecordType) {
-    ERecordType["MOVIE"] = "movie";
-    ERecordType["TV_SERIES"] = "tvseries";
-})(ERecordType = exports.ERecordType || (exports.ERecordType = {}));
